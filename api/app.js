677beeca9f4ed37ab5b04e5855af9a9c45c1a6cb
AlinaSyniavska <alina22syniavska@gmail.com>
2024-03-06 15:05:14 +0200

    Fixed CORS configuration

--- api/app.js
+++ api/app.js
@@ -4,7 +4,6 @@ dotenv.config();
 import express from 'express';
 import cors from 'cors';
 
-import { config } from './configs/index.js';
 import { authRouter, userRouter } from './routes/index.js';
 
 const app = express();
@@ -13,7 +12,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.options('*', cors()); // include before other routes
-app.use(cors(_configureCors()));
+// app.use(cors(_configureCors()));
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    credentials: true
+}));
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', "http://localhost:5173");
+    res.header('Access-Control-Allow-Headers', true);
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    next();
+});
 
 app.use('/auth', authRouter);
 // app.use('/chats', chatsRouter);
@@ -35,14 +46,14 @@ app.listen(process.env.PORT, () => {
 });
 
 function _configureCors() {
-    const whitelist = config.CORS_WHITE_LIST.split(';');
+    const whitelist = process.env.CORS_WHITE_LIST.split(';');
 
     return {
         origin: (origin, callback) => {
             if (whitelist.includes(origin) || !origin) {
                 callback(null, true);
             } else {
-                callback(new Error('Not allowed by CORS'));
+                callback(new Error('Not allowed by CORS 2222'));
             }
         },
     };
